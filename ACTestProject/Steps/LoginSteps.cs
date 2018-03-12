2ac146bfadfefb2bf4e2e4e68baf92c95eef3c6b
Felipe Luz <felipegluz@hotmail.com>
2018-03-12 04:42:55 -0300

    Projeto Completo

---
+++ ACTestProject/Steps/LoginSteps.cs
@@ -0,0 +1,48 @@
+﻿using ACTestProject.Pages;
+using TechTalk.SpecFlow;
+
+namespace ACTestProject.Steps
+{
+    [Binding, Scope(Feature ="Login", Tag = "login")]    
+    class LoginSteps
+    {
+        //[BeforeTestRun]
+        //public void Initialize()
+        //{
+        //    Browser.Initialize();
+        //}
+
+        //[AfterTestRun]
+        //public void TearDown()
+        //{
+        //    Browser.TearDown();
+        //}
+
+        LoginPage page = new LoginPage();
+
+        [Given(@"I have navigated to the website")]
+        public void GivenIHaveNavigatedToTheWebsite()
+        {
+            Browser.Initialize();
+        }             
+
+        [When(@"I type '(.*)' and '(.*)'")]
+        public void WhenITypeAnd(string email, string password)
+        {
+            page.Login(email, password);
+        }
+
+        [When(@"I click on the Sign In button")]
+        public void WhenIClickOnTheSignInButton()
+        {            
+            PropertiesCollection.currentPage = page.ClickSignInButton();
+        }
+        
+        [Then(@"I should see the ToDoApp page")]
+        public void ThenIShouldSeeTheToDoAppPage()
+        {
+            //Casting
+            ((HomePage)PropertiesCollection.currentPage).isLoggedIn();            
+        }
+    }
+}
